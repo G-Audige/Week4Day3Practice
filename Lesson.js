5305ad29ea7be27ab40b975c46c0aa4dd6789d05
G-Audige <gloriaaudige@gmail.com>
2023-10-26 11:26:28 -0400

    Where is Waldo answered

--- Lesson.js
+++ Lesson.js
@@ -6,6 +6,7 @@
 //     console.log(i)
 // }
 
+
 //++++++++++++++++++++++++++++++//
 //            Get Even          //
 //++++++++++++++++++++++++++++++//
@@ -13,6 +14,7 @@
 // for(let i = 0; i <= 200; i += 2) {
 //     console.log(i)
 // }
+
 
 //++++++++++++++++++++++++++++++//
 //           Fizz Bizz          //
@@ -36,6 +38,7 @@
 //     }
 // }
 
+
 //++++++++++++++++++++++++++++++//
 //         Wild Wild West       //
 //++++++++++++++++++++++++++++++//
@@ -48,7 +51,6 @@
 // const plantee = ["Plantee", "plant",  5000 , "Mordor"]
 // const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
 // const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
-
 // plantee[2] = 5001
 // wolfy[3] = "Gotham City"
 // dart.push("Hawkins")
@@ -56,6 +58,7 @@
 // console.log(plantee)
 // console.log(wolfy)
 // console.log(dart)
+
 
 //++++++++++++++++++++++++++++++//
 //   Yell at the Ninja Turtles  //
@@ -67,57 +70,70 @@
 //     console.log(cap.toUpperCase())
 // }
 
+
 //++++++++++++++++++++++++++++++//
 //       Methods, Revisited     //
 //++++++++++++++++++++++++++++++//
-const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 // 1. use the .sort method 
-favMovies.sort()
+// favMovies.sort()
     //Thought question: what did this do to the array? 
         // The .sort method sorts the elements of an array and returns them to the same array
     //Did it permanently alter it?
         // Yes
 // 2. Use the method pop
-favMovies.pop()
+// favMovies.pop()
 // 3. push"Guardians of the Galaxy"
-favMovies.push("Guardians of the Galaxy")
+// favMovies.push("Guardians of the Galaxy")
 // 4. Reverse the array
-favMovies.reverse()
+// favMovies.reverse()
 // 5. Use the shift method
-favMovies.shift()
+// favMovies.shift()
 // 6. unshift-
-console.log(favMovies.unshift()) 
+// console.log(favMovies.unshift()) 
     // what does it return?
         // It returns the length property of the array
 // 7. splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) 
     // Thought question: did this permanently alter our array?
         // Yes
-console.log(favMovies.findIndex(find => {
-    return find === "Django Unchained"
-}))
-favMovies.splice(14,1,"Avatar")
+// console.log(favMovies.findIndex(find => {
+//     return find === "Django Unchained"
+// }))
+// favMovies.splice(14,1,"Avatar")
 // 8. slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - 
     // Thought question: did this permanently alter our array?
         // No
-favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
+// favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
 // 9. store the value of your slice in a variable, console.log it - 
     // Thought question: what is going on here?
-const halfFavMovies = favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
-console.log(halfFavMovies)
+// const halfFavMovies = favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
+// console.log(halfFavMovies)
 // 10. console.log your final results
-console.log(favMovies)
+// console.log(favMovies)
 // 11. After running the above tasks, console.log the index of "Fast and Furious" 
     //-We removed it from the array, what value do we get when we look for the index of something that is not in the array?
         // Fast and Furious was never removed from the array
-console.log(favMovies.findIndex(find => {
-    return find === "Fast and Furious"
-}))
+// console.log(favMovies.findIndex(find => {
+//     return find === "Fast and Furious"
+// }))
 // 12. Thought question: that we declared the variable favMovieswith const, and yet, we were allowed to change the array. Weird? Should we have used let?
     // Using const or let gives the same results for what was done with this array
+
 
 //++++++++++++++++++++++++++++++//
 //         Where is Waldo       //
 //++++++++++++++++++++++++++++++//
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+//Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.splice(1,1)
+console.log(whereIsWaldo)
+// Change "Neff" to "No One"
+whereIsWaldo[1][2] = "No One"
+// Access and console.log "Waldo"
+console.log(whereIsWaldo[2][1][1])
+
 
 //++++++++++++++++++++++++++++++//
 //        Excited Kitten        //
