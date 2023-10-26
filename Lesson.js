05f541becb894234d1ad47427323a0494370c56c
G-Audige <gloriaaudige@gmail.com>
2023-10-26 11:49:44 -0400

    Final push

--- Lesson.js
+++ Lesson.js
@@ -2,18 +2,18 @@
 //           Easy Going         //
 //++++++++++++++++++++++++++++++//
 // Write a for loop that will log the numbers 1 through 20.
-// for(let i = 1; i <= 20; i++) {
-//     console.log(i)
-// }
+for(let i = 1; i <= 20; i++) {
+    console.log(i)
+}
 
 
 //++++++++++++++++++++++++++++++//
 //            Get Even          //
 //++++++++++++++++++++++++++++++//
 // Write a for loop that will log only the even numbers in 0 through 200.
-// for(let i = 0; i <= 200; i += 2) {
-//     console.log(i)
-// }
+for(let i = 0; i <= 200; i += 2) {
+    console.log(i)
+}
 
 
 //++++++++++++++++++++++++++++++//
@@ -23,20 +23,20 @@
 // 2. If a number is divisible by 3 log "Fizz" instead of the number.
 // 3. If a number is divisible by 5 log "Buzz" instead of the number.
 // 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
-// for(let i = 1;i <= 100; i++) {
-//     if(i % 3 === 0 || i % 5 === 0) {
-//         console.log("FizzBuzz")
-//     }
-//     else if(i % 3 === 0) {
-//         console.log("Fizz")
-//     }
-//     else if(i % 5 === 0) {
-//         console.log("Buzz")
-//     }
-//     else {
-//         console.log(i)
-//     }
-// }
+for(let i = 1;i <= 100; i++) {
+    if(i % 3 === 0 || i % 5 === 0) {
+        console.log("FizzBuzz")
+    }
+    else if(i % 3 === 0) {
+        console.log("Fizz")
+    }
+    else if(i % 5 === 0) {
+        console.log("Buzz")
+    }
+    else {
+        console.log(i)
+    }
+}
 
 
 //++++++++++++++++++++++++++++++//
@@ -46,18 +46,18 @@
 // 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
 // 3. Give D'Art a second hometown by adding "Hawkins"
 // 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-// const sharky = ["Sharky", "shark", 20, "Left Coast"]
-// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
-// plantee[2] = 5001
-// wolfy[3] = "Gotham City"
-// dart.push("Hawkins")
-// wolfy.splice(0,1,"Gameboy")
-// console.log(plantee)
-// console.log(wolfy)
-// console.log(dart)
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+plantee[2] = 5001
+wolfy[3] = "Gotham City"
+dart.push("Hawkins")
+wolfy.splice(0,1,"Gameboy")
+console.log(plantee)
+console.log(wolfy)
+console.log(dart)
 
 
 //++++++++++++++++++++++++++++++//
@@ -65,10 +65,10 @@
 //++++++++++++++++++++++++++++++//
 // 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
 // 2. Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase() on each of them and print out the result.
-// const ninTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
-// for(let cap of ninTurtles) {
-//     console.log(cap.toUpperCase())
-// }
+const ninTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+for(let cap of ninTurtles) {
+    console.log(cap.toUpperCase())
+}
 
 
 //++++++++++++++++++++++++++++++//
@@ -76,46 +76,46 @@
 //++++++++++++++++++++++++++++++//
 // const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 // 1. use the .sort method 
-// favMovies.sort()
+favMovies.sort()
     //Thought question: what did this do to the array? 
         // The .sort method sorts the elements of an array and returns them to the same array
     //Did it permanently alter it?
         // Yes
 // 2. Use the method pop
-// favMovies.pop()
+favMovies.pop()
 // 3. push"Guardians of the Galaxy"
-// favMovies.push("Guardians of the Galaxy")
+favMovies.push("Guardians of the Galaxy")
 // 4. Reverse the array
-// favMovies.reverse()
+favMovies.reverse()
 // 5. Use the shift method
-// favMovies.shift()
+favMovies.shift()
 // 6. unshift-
-// console.log(favMovies.unshift()) 
+console.log(favMovies.unshift()) 
     // what does it return?
         // It returns the length property of the array
 // 7. splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) 
     // Thought question: did this permanently alter our array?
         // Yes
-// console.log(favMovies.findIndex(find => {
-//     return find === "Django Unchained"
-// }))
-// favMovies.splice(14,1,"Avatar")
+console.log(favMovies.findIndex(find => {
+    return find === "Django Unchained"
+}))
+favMovies.splice(14,1,"Avatar")
 // 8. slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - 
     // Thought question: did this permanently alter our array?
         // No
-// favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
+favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
 // 9. store the value of your slice in a variable, console.log it - 
     // Thought question: what is going on here?
-// const halfFavMovies = favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
-// console.log(halfFavMovies)
+const halfFavMovies = favMovies.slice(Math.floor(favMovies.length / 2), favMovies.length)
+console.log(halfFavMovies)
 // 10. console.log your final results
-// console.log(favMovies)
+console.log(favMovies)
 // 11. After running the above tasks, console.log the index of "Fast and Furious" 
     //-We removed it from the array, what value do we get when we look for the index of something that is not in the array?
         // Fast and Furious was never removed from the array
-// console.log(favMovies.findIndex(find => {
-//     return find === "Fast and Furious"
-// }))
+console.log(favMovies.findIndex(find => {
+    return find === "Fast and Furious"
+}))
 // 12. Thought question: that we declared the variable favMovieswith const, and yet, we were allowed to change the array. Weird? Should we have used let?
     // Using const or let gives the same results for what was done with this array
 
@@ -123,16 +123,16 @@
 //++++++++++++++++++++++++++++++//
 //         Where is Waldo       //
 //++++++++++++++++++++++++++++++//
-// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-//                     ["Lucinda", "Jacc", "Neff", "Snoop"],
-//                     ["Petunia", ["Baked Goods", "Waldo"]]];
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
 //Remove Eggbert (hint look at the slice/splice method(s))
-// whereIsWaldo.splice(1,1)
-// console.log(whereIsWaldo)
+whereIsWaldo.splice(1,1)
+console.log(whereIsWaldo)
 // Change "Neff" to "No One"
-// whereIsWaldo[1][2] = "No One"
+whereIsWaldo[1][2] = "No One"
 // Access and console.log "Waldo"
-// console.log(whereIsWaldo[2][1][1])
+console.log(whereIsWaldo[2][1][1])
 
 
 //++++++++++++++++++++++++++++++//
@@ -141,14 +141,14 @@
 // 1. Write code that logs "Love me, pet me! HISSSSSS!" 20 times.
 // 2. For every even number in your loop, log "...human...why you taking pictures of me?...", c, or "...why does the red dot always get away..." at random.
 // const questions = ["0 ...human...why you taking pictures of me?...", "1 ...the catnip made me do it...", "2 ...why does the red dot always get away..."]
-// for(let i = 1; i <= 20; i++) {
-//     if(i % 2 === 0) {
-//         console.log("Love me, pet me! HISSSSSS!")
-//     }
-//     else {
-//         console.log(questions[Math.floor(Math.random() * 3)])
-//     }
-// }
+for(let i = 1; i <= 20; i++) {
+    if(i % 2 === 0) {
+        console.log("Love me, pet me! HISSSSSS!")
+    }
+    else {
+        console.log(questions[Math.floor(Math.random() * 3)])
+    }
+}
 
 
 //++++++++++++++++++++++++++++++//
